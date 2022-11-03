4cb9083b5ea6f849e33e6ea54ad2ef059d78d986
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-11-03 10:41:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,5 @@ with st.expander('Show list of dataset'):
     st.write(data())
 
 st.subheader(f'Selected data (`{selected_data}`)')
-st.info(data().title)
+st.info(selected_data.title)
 st.write(data(selected_data))
