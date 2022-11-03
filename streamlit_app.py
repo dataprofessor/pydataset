98bf567383cce190adde8033b52f5a3fe861f7a4
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-11-03 11:01:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,13 @@ from pydataset import data
 st.title('🎈 pydataset')
 
 selected_data = st.sidebar.selectbox('Select a dataset', data().dataset_id)
-data()[data()['dataset_id'] == selected_data]['title']
+#data()[data()['dataset_id'] == selected_data]['title']
+data()
 
-st.header('Datasets')
-st.subheader('List of dataset')
-with st.expander('Show list of dataset'):
-    st.write(data())
+#st.header('Datasets')
+#st.subheader('List of dataset')
+#with st.expander('Show list of dataset'):
+#    st.write(data())
 
-st.subheader(f'Selected data (`{selected_data}`)')
-st.write(data(selected_data))
+#st.subheader(f'Selected data (`{selected_data}`)')
+#st.write(data(selected_data))
