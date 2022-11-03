58c5cb05cbf1d2234b6a943fda9c93bc40e64e54
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-11-03 10:53:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 import streamlit as st
+import pandas as pd
 from pydataset import data
 
 st.title('🎈 pydataset')
 
 selected_data = st.sidebar.selectbox('Select a dataset', data().dataset_id)
-data_description = pd.DataFrame(data())[dataset_id]
+data_description = data()[dataset_id]
 
 st.header('Datasets')
 st.subheader('List of dataset')
